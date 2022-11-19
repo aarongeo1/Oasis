e3b54eaad8e48a0a47bdc352786325eb7a1415c4
JanukanS <28988453+JanukanS@users.noreply.github.com>
2022-11-19 15:41:51 -0700

    Added plotting and data processing for Edmonton

--- data/plotting.py
+++ data/plotting.py
@@ -3,33 +3,37 @@ import numpy as np
 import pandas as pd
 from itertools import product
 
-calFrame = pd.read_csv('cleandata/calgary.csv')
-longitude = calFrame['longitude'].values
-latitude = calFrame['latitude'].values
+def makeFDPlot(sourceFile, city, longkey, latkey):
+    dataFrame = pd.read_csv(sourceFile)
+    longitude = dataFrame[longkey].values
+    latitude = dataFrame[latkey].values
 
-longrange = [min(longitude), max(longitude)]
-latrange = [min(latitude), max(latitude)]
+    longrange = [min(longitude), max(longitude)]
+    latrange = [min(latitude), max(latitude)]
 
-longval = np.linspace(*longrange, 500)
-latval = np.linspace(*latrange, 500)
+    longval = np.linspace(*longrange, 500)
+    latval = np.linspace(*latrange, 500)
 
-longmesh, latmesh = np.meshgrid(longval, latval, indexing='ij')
+    longmesh, latmesh = np.meshgrid(longval, latval, indexing='ij')
 
-distMesh = np.zeros(np.shape(longmesh))
+    distMesh = np.zeros(np.shape(longmesh))
 
-for i,j in product(range(len(longval)),range(len(latval))):
-    loDist = longitude-longmesh[i][j]
-    laDist = latitude - latmesh[i][j]
-    dist = np.sqrt(loDist**2 + laDist**2)
-    distMesh[i][j] = np.min(dist)
-plt.imshow(distMesh,
-           interpolation='spline36',
-           origin='lower',
-           cmap=plt.cm.get_cmap('viridis_r'),
-           extent=longrange+latrange)
-plt.xlabel('Longitude')
-plt.ylabel('Latitude')
-plt.title('Distance From Nearest Grocery Store - Calgary')
-cbar = plt.colorbar()
-cbar.ax.set_ylabel('Coordinate Distance')
-plt.show()
+    for i,j in product(range(len(longval)),range(len(latval))):
+        loDist = longitude-longmesh[i][j]
+        laDist = latitude - latmesh[i][j]
+        dist = np.sqrt(loDist**2 + laDist**2)
+        distMesh[i][j] = np.min(dist)
+    plt.imshow(distMesh,
+               interpolation='spline36',
+               origin='lower',
+               cmap=plt.cm.get_cmap('viridis_r'),
+               extent=longrange+latrange)
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+    plt.title(f'Distance From Nearest Grocery Store - {city}')
+    cbar = plt.colorbar()
+    cbar.ax.set_ylabel('Coordinate Distance')
+    plt.savefig(f'plots/{city}Plot.jpg')
+
+makeFDPlot('cleandata/calgary.csv', 'Calgary', 'longitude', 'latitude')
+makeFDPlot('cleandata/edmonton.csv', 'Edmonton', 'Longitude', 'Latitude')
--- data/processData.py
+++ data/processData.py
@@ -1,12 +1,10 @@
 import pandas as pd
-from functools import reduce
+from functools import reduce, partial
 
-# Load Data
-calFrame = pd.read_csv("rawdata/calgaryData.csv")
-edmFrame = pd.read_csv("rawdata/edmontonData.csv")
 
-def checkName(frame, name):
-    return frame['TRADENAME'].str.contains(name)
+
+def checkKeyName(key,frame, name):
+    return frame[key].str.contains(name)
 
 def AndBoolSeries(seriesList):
     singleAnd = lambda x, y: (x) & (y)
@@ -20,37 +18,71 @@ def NorBoolSeries(seriesList):
     singleNor = lambda x, y: ~(x) & ~(y)
     return (reduce(singleNor, seriesList))
 
-def checkManyNames(frame, andList, orList, exclList):
+def checkManyKeyNames(checkKeyNameFunc, frame, andList, orList, exclList):
     finalSeriesList = []
     if len(andList) != 0:
-        andSeries = AndBoolSeries([checkName(frame, andTerm) for andTerm in andList])
+        andSeries = AndBoolSeries([checkKeyNameFunc(frame, andTerm) for andTerm in andList])
         finalSeriesList.append(andSeries)
     if len(orList) != 0:
-        orSeries = OrBoolSeries([checkName(frame, orTerm) for orTerm in orList])
+        orSeries = OrBoolSeries([checkKeyNameFunc(frame, orTerm) for orTerm in orList])
         finalSeriesList.append(orSeries)
     if len(exclList) != 0:
-        exclSeries = NorBoolSeries([checkName(frame, exclTerm) for exclTerm in exclList])
+        exclSeries = NorBoolSeries([checkKeyNameFunc(frame, exclTerm) for exclTerm in exclList])
         finalSeriesList.append(exclSeries)
 
     completeSeries = AndBoolSeries(finalSeriesList)
     return frame[completeSeries]
 
-foodCategory = calFrame['LICENCETYPES'] == "FOOD SERVICE - PREMISES"
-foodFrame = calFrame[foodCategory]
+def processCalgary():
+    checkName = partial(checkKeyName, 'TRADENAME')
+    checkManyNames = partial(checkManyKeyNames, checkName)
 
-rcssFrame = checkManyNames(foodFrame, ['CANADIAN SUPERSTORE'], [], [])
-safewayFrame = checkManyNames(foodFrame, ['SAFEWAY'], [], ['SAFEWAY GAS', 'SAFEWAY LIQUOR'])
-walmartFrame = checkManyNames(foodFrame, ['WAL-MART'], [], [])
-sobeysFrame = checkManyNames(foodFrame, ['SOBEYS'], [], [])
-coopFrame = checkManyNames(foodFrame, ['CO-OP'], [], ['CO-OP WINE', 'GAS BAR'])
-supermarketFrame = checkManyNames(foodFrame, ['SUPERMARKET'], [], [])
-martFrame = checkManyNames(foodFrame, ['GROCER'],[],[])
+    calFrame = pd.read_csv("rawdata/calgaryData.csv")
+    foodCategory = calFrame['LICENCETYPES'] == "FOOD SERVICE - PREMISES"
+    foodFrame = calFrame[foodCategory]
 
-finalCalgary = pd.concat([rcssFrame,
-                          safewayFrame,
-                          walmartFrame,
-                          sobeysFrame,
-                          coopFrame,
-                          supermarketFrame,
-                          martFrame],axis=0)
-finalCalgary.to_csv('cleandata/calgary.csv',index=False)
+    rcssFrame = checkManyNames(foodFrame, ['CANADIAN SUPERSTORE'], [], [])
+    safewayFrame = checkManyNames(foodFrame, ['SAFEWAY'], [], ['SAFEWAY GAS', 'SAFEWAY LIQUOR'])
+    walmartFrame = checkManyNames(foodFrame, ['WAL-MART'], [], [])
+    sobeysFrame = checkManyNames(foodFrame, ['SOBEYS'], [], [])
+    coopFrame = checkManyNames(foodFrame, ['CO-OP'], [], ['CO-OP WINE', 'GAS BAR'])
+    supermarketFrame = checkManyNames(foodFrame, ['SUPERMARKET'], [], [])
+    martFrame = checkManyNames(foodFrame, ['GROCER'],[],[])
+
+    finalCalgary = pd.concat([rcssFrame,
+                              safewayFrame,
+                              walmartFrame,
+                              sobeysFrame,
+                              coopFrame,
+                              supermarketFrame,
+                              martFrame],axis=0)
+    finalCalgary.to_csv('cleandata/calgary.csv',index=False)
+
+def processEdmonton():
+    edmFrame = pd.read_csv("rawdata/edmontonData.csv")
+    checkName = partial(checkKeyName, 'Trade Name')
+    checkManyNames = partial(checkManyKeyNames, checkName)
+    foodFrame = edmFrame[(edmFrame['Category'] == 'Major Retail Store')]
+
+    rcssFrame = checkManyNames(foodFrame, ['CANADIAN SUPERSTORE'], [], [])
+    safewayFrame = checkManyNames(foodFrame, ['SAFEWAY'], [], ['SAFEWAY GAS', 'SAFEWAY LIQUOR'])
+    walmartFrame = checkManyNames(foodFrame, ['WAL-MART'], [], [])
+    sobeysFrame = checkManyNames(foodFrame, ['SOBEYS'], [], [])
+    coopFrame = checkManyNames(foodFrame, ['CO-OP'], [], ['CO-OP WINE', 'GAS BAR'])
+    supermarketFrame = checkManyNames(foodFrame, ['SUPERMARKET'], [], [])
+    martFrame = checkManyNames(foodFrame, ['GROCER'],[],[])
+
+    finalEdmonton = pd.concat([rcssFrame,
+                              safewayFrame,
+                              walmartFrame,
+                              sobeysFrame,
+                              coopFrame,
+                              supermarketFrame,
+                              martFrame],axis=0)
+    #print(finalEdmonton['Trade Name'])
+    #print(len(finalEdmonton.index))
+    #print(len(foodFrame.index))
+    finalEdmonton.to_csv('cleandata/edmonton.csv', index=False)
+processEdmonton()
+processCalgary()
+
